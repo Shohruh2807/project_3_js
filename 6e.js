c76c83c7a0ed2ba04903d6680806bf7be637883a
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-07-10 22:47:59 +0500

    6e commit

---
+++ 6e.js
@@ -0,0 +1,9 @@
+'use strict'
+let yosh=14
+let isHoliday = true
+if (isHoliday|| isHoliday ===false && (6>=yosh||65<=yosh)){
+    console.log("chegirma")
+}
+else{
+    console.log("chegirma yo\'q,battar bo\'l")
+}
