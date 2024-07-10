54ed256e676b3dfe5b65404719764cfd56040295
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-07-10 22:18:43 +0500

    6a commit

---
+++ 6a.js
@@ -0,0 +1,11 @@
+'use strict'
+let soat=23
+if (6<=soat<=12){
+    console.log("Xayrli tong!")
+}
+else if(13<=soat<=17){
+    console.log("Xayrli kun!")
+}
+else{
+    console.log("Xayrli tun!")
+}
