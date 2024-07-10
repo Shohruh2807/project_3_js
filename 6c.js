eeb570614dcf970dfaf26076df7dbabddf3864d9
Shohruh2807 <jalilovshohruh2010@gmail.com>
2024-07-10 22:33:18 +0500

    6c commit

---
+++ 6c.js
@@ -0,0 +1,13 @@
+'use strict'
+let soat=23
+if (6<=soat<=12){
+    console.log("Xayrli tong!")
+}
+else if(13<=soat<=17){
+    console.log("Xayrli kun!")
+}
+else{
+    console.log("Xayrli tun!")
+}
+let name="Shohruh"
+console.log(`${soat} ${name}`)
